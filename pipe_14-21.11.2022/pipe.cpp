ae0d232c2dbec61b72c5f1cc5db8b93aad13da72
Karen0student <arzkaren4@gmail.com>
2022-11-21 11:53:07 +0400

    optimized code

--- pipe_14-21.11.2022/pipe.cpp
+++ pipe_14-21.11.2022/pipe.cpp
@@ -1,56 +1,51 @@
-/*Напишите программу, в которой родительский процесс порождает
- две дочерние процессы, которые должны обменяться своими pid.
- Обмен должен быть реализован с помощью каналов. */
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <cstring>
 
-int main(){
-    int pipefd[2];
-    int pipe_res = pipe(pipefd);
+int main(int argc, char** argv){
+    int fd1[2];
+    int fd2[2];
+
+    if(pipe(fd1) == -1){
+        std::cout << "ERROR" << std::endl;
+    }
+    if(pipe(fd2) == -1){
+        std::cout << "ERROR" << std::endl;
+    }
+    pid_t child1, child2;
+    (child1 = fork()) && (child2 = fork());
+
+    if(child1 == 0){
+        close(fd1[0]);
+        int pid_of_child1 = getpid();
+        write(fd1[1], &pid_of_child1, sizeof(int));
+        close(fd1[1]);
+
+        close(fd2[1]);
+        int recieved_pid_of_child1;
+        read(fd2[0], &recieved_pid_of_child1, sizeof(int));
+        close(fd2[0]);
+        std::cout << "First: my pid is: " << pid_of_child1 << std::endl;
+    }
+    else if(child2 == 0){
+        close(fd2[0]);
+        int pid_of_child2 = getpid();
+        write(fd2[1], &pid_of_child2, sizeof(int));
+        close(fd2[1]);
+
+        close(fd1[1]);
+        int recieved_pid_of_child2;
+        read(fd1[0], &recieved_pid_of_child2, sizeof(int));
+        close(fd1[0]);
+        std::cout << "Second: my pid is: " << pid_of_child2 << std::endl;
+    }
+
+    else{
+        if(wait(NULL) == -1){
+            std::cout << "ERROR(wait)" << std::endl;
+            exit(0);
+        }
+    }
+
     
-    pid_t pid = fork();
-    if(pid < 0){
-        std::cout << "ERROR(fork_pid)\n";
-        exit(0);
-    }
-    pid_t pid2 = fork();
-    if(pid < 0){
-        std::cout << "ERROR(fork_pid2)\n";
-        exit(0);
-    }
-
-    if(pid == 0 && pid2 == 0){ //child + child^2
-        std::cout << "ENTERED CHILD" << std::endl;
-        int written = write(pipefd[1], &pid, sizeof(pid_t));
-        if(written < 0){
-            std::cout << "ERROR(written)" << std::endl;
-            exit(0);
-        }
-        int written2 = write(pipefd[1], &pid2, sizeof(pid_t));
-        if(written2 < 0){
-            std::cout << "ERROR(written)" << std::endl;
-            exit(0);
-        }
-        int readBytes2 = read(pipefd[0], &pid2, sizeof(pid_t));
-        if(readBytes2 < 0){
-            std::cout << "ERROR(readBytes2)" << std::endl;
-            exit(0);
-        }
-        int readBytes = read(pipefd[0], &pid, sizeof(pid_t));
-        if(readBytes < 0){
-            std::cout << "ERROR(readbytes)" << std::endl;
-            exit(0);
-        }
-        _exit(0);
-    }
-
-    else{ //parent
-        int status;
-        waitpid(pid2, &status, 0);
-        //waitpid(pid, &status, 0);
-        std::cout << "***finished***" << std::endl;
-        }
-        
-}
+} 
