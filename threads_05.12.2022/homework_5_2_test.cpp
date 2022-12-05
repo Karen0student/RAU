3ccaa4e8aa957c6596a391000fcb06e511e5d410
Karen0student <arzkaren4@gmail.com>
2022-12-05 16:27:52 +0400

    added struct and improved if_else

--- threads_05.12.2022/homework_5_2_test.cpp
+++ threads_05.12.2022/homework_5_2_test.cpp
@@ -7,11 +7,19 @@
 #include <cmath>
 #include <cstring>
 
+struct thread_data{
+    int index;
+    int num1;
+    int num2;
+    std::string symbol;
+};
 
 void* threadFunc(void* arg){
+    thread_data* args = (thread_data*) arg;
     std::cout << "entered threadFunc" << std::endl;
     int fd = 0;
-    std::string file_name = "out_" + ((std::string*)arg)[0] + ".txt";
+    //std::cout << args->num1  << " " << args->num2 << std::endl;
+    std::string file_name = "out_" + std::to_string(args->index) + ".txt";
     std::cout << file_name << std::endl;
     fd = open(file_name.c_str(), O_TRUNC | O_CREAT | O_RDWR, 0755);
     if(fd < 0){
@@ -20,19 +28,20 @@ void* threadFunc(void* arg){
     }
     std::cout << "after opening file" << std::endl;
     
-    int x = ((int*)arg)[1];
-    int y = ((int*)arg)[2];
     
+    // int x = ((int*)arg)[1];
+    // int y = ((int*)arg)[2];
+//change args to args
     //check
-    std::cout << "arg[1] = " << ((std::string*)arg)[1] << " arg[2] = " << ((std::string*)arg)[2] << std::endl;
-    std::cout << "x = " << x << " y = " << y << std::endl;
+    //std::cout << "arg[1] = " << ((std::string*)arg)[1] << " arg[2] = " << ((std::string*)arg)[2] << std::endl;
+    std::cout << "x = " << args->num1 << " y = " << args->num2 << std::endl;
     //
 
     int fda;
-    std::cout << ((std::string*)arg)[3] << std::endl;
+    std::cout << args->symbol << std::endl;
     //const std::string* symbol = (((std::string*)arg)[3]).c_str(); 
-        if(((std::string*)arg)[3] == "s"){
-            std::string str = ((std::string*)arg)[1] + " s " + ((std::string*)arg)[2] + " = " + std::to_string(x + y);
+        if(args->symbol == "s"){
+            std::string str = std::to_string(args->num1) + " s " + std::to_string(args->num2) + " = " + std::to_string(args->num1 + args->num2);
             std::cout << str.size() << std::endl;
             fda = write(fd, str.c_str(), str.size());
             if(fda < 0){
@@ -45,8 +54,8 @@ void* threadFunc(void* arg){
             }
             pthread_exit(NULL);
         }
-        if(((std::string*)arg)[3] == "m"){// *
-            std::string str = std::to_string(x) + " m " + std::to_string(y) + " = " + std::to_string(x * y);
+        if(args->symbol == "m"){// *
+            std::string str = std::to_string(args->num1) + " m " + std::to_string(args->num2) + " = " + std::to_string(args->num1 * args->num2);
             fda = write(fd, str.c_str(), str.size());
             if(fda < 0){
                 std::cout << "ERROR(*)" << std::endl;
@@ -58,8 +67,8 @@ void* threadFunc(void* arg){
             }
             pthread_exit(NULL);
         }
-        if(((std::string*)arg)[3] == "ss"){// ^, +
-            std::string str = std::to_string(x) + " s " + std::to_string(y) + " = " + std::to_string(pow(x, x) + pow(y, y));
+        if(args->symbol == "ss"){// ^, +
+            std::string str = std::to_string(args->num1) + " s " + std::to_string(args->num2) + " = " + std::to_string(pow(args->num1, args->num1) + pow(args->num2, args->num2));
             fda = write(fd, str.c_str(), str.size());
             if(fda < 0){
                 std::cout << "ERROR(+)" << std::endl;
@@ -83,33 +92,21 @@ void* threadFunc(void* arg){
 }
 
 int main(int argc, char** argv){
-    // if(atoi(argv[1]) <= 0 || argc <= 1){
-    //     std::cout << "***program finished, nothing passed***" << std::endl;
-    //     exit(0);
-    // }
     pthread_t tid;
     for(int i = 0; i < atoi(argv[1]); ++i){   
         std::cout << "entered cycle" << std::endl;
-        //char* arg[1] = {(char*)i};
-        // char* arg = new char;
-        // arg = (char*)i;
-        std::cout << "before thread" << std::endl;
-        std::string x, y, argument;
-        std::cin >> x >> y >> argument;
 
-        std::string arg [4] = {std::to_string(i), x, y, argument};
-        int threadRes = pthread_create(&tid, NULL, &threadFunc, (std::string*)&arg);
+        std::cout << "before thread" << std::endl;
+        thread_data object;
+        object.index = i;
+        std::cin >> object.num1 >> object.num2 >> object.symbol;
+
+        int threadRes = pthread_create(&tid, NULL, threadFunc, (void*)&object);
         if(threadRes != 0){
             std::cout << "Could not create thread\n";
             exit(0);
         }
         void* retval;
         int join = pthread_join(tid, &retval);
-        //delete arg;
     }
-
-    // for(int i = 0; i < atoi(argv[1]); ++i){
-        
-    // }
-
 }
